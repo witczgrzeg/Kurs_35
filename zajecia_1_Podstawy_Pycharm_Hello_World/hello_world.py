20969cb9f8e133e1abef64bdd0cf6ee1f00e4dba
witczgrzeg <grzegorz.witczak.gw@gmail.com>
2025-05-16 07:56:02 +0200

    porządki - zmiany nazw folderów i dodanie zajęć 8

---
+++ zajecia_1_Podstawy_Pycharm_Hello_World/hello_world.py
@@ -0,0 +1,119 @@
+# To jest nasz komentarz
+
+#print("Hello World")
+
+################## INTEGERS AND FLOATS ##################
+# wiek = 20
+# print(wiek)
+# print(type(wiek))
+# wiek1 = 20 + 5
+# print(wiek1)
+# operacja_odejmowania = 20 - 5
+# print(operacja_odejmowania)
+# wiek2 = 20 * 5
+# print(wiek2)
+# wiek3 = 20 / 5
+# print(wiek3)
+# print(type(wiek3))
+# wiek_do_kwadratu = 20 ** 2
+# print(wiek_do_kwadratu)
+# modulo_z_dzielenia = 20 % 3
+# print(modulo_z_dzielenia)
+
+# liczba_1 = 20
+# liczba_2 = 10
+#
+# suma = liczba_1 + liczba_2
+# print(suma)
+
+
+#################### STRINGS ####################
+
+# imie = "Michal"
+# print(imie)
+#
+# imie_i_nazwisko = "Michal Zietkowski"
+# print(imie_i_nazwisko)
+#
+# dane_osobowe = imie_i_nazwisko + " 20 lat"
+# print(dane_osobowe)
+#
+# dlugi_string = """
+#     Litania do sw. Judy Tadeusza
+#     Swiety Judy Tadeuszu, Apostole i Patronie,
+#     wstawiaj sie za nami u Boga,
+#     aby nasza modlitwa byla wysluchana.
+# """
+#
+# print(dlugi_string)
+#
+# imie_i_nazwisko_z_enterem = "Michal\nZietkowski"
+# print(imie_i_nazwisko_z_enterem)
+#
+# imie_i_nazwisko = "miChal zietkowski"
+#
+# print(imie_i_nazwisko.capitalize())
+
+
+#################### BOOL ####################
+# wartosc_prawdziwa = True
+# wartosc_falszywa = False
+#
+# print(wartosc_prawdziwa)
+# print(wartosc_falszywa)
+
+#
+# wiek_michala = 30
+# wiek_adama = 20
+#
+# wiek_jest_rozny = wiek_michala != wiek_adama
+# wiek_jest_rowny = wiek_michala == wiek_adama
+# # print(wiek_adama == wiek_michala)
+# # print(wiek_adama != wiek_michala)
+# # print(wiek_adama > wiek_michala)
+# # print(wiek_adama <= wiek_michala)
+# print(wiek_jest_rozny)
+# print(wiek_jest_rowny)
+
+#################### NONE ####################
+
+# zmienna_nieokreslona = None
+# print(type(zmienna_nieokreslona))
+#
+# zmienna_nieokreslona = 20
+# print(type(zmienna_nieokreslona))
+
+
+###################### FORMAT STRING ######################
+
+
+# imie = "Michal"
+# nazwisko = "Zietkowski"
+# print("Twoje imie to: {imie_wartosc}, a nazwisko to: {naziwsko_wartosc}".format(imie_wartosc=imie, naziwsko_wartosc=nazwisko))
+#
+# print(f"Twoje imie to {imie}, a nazwisko to: {nazwisko}")
+
+
+################ STANDARDOWE WEJSCIE ################
+#
+# imie = input("Podaj swoje imie: ")
+# print(imie)
+# print(type(imie))
+# wiek = input("Podaj swoj wiek: ")
+# print(wiek)
+# print(type(wiek))
+# wiek_w_integerze = int(wiek)
+#
+# print(f"Do 25 lat brakuje Ci {25 - wiek_w_integerze} lat")
+#
+# dwadziescia_int = int("twenty")
+dwadziescia_str = str(20)
+print(dwadziescia_str)
+print(type(dwadziescia_str))
+print(bool(1))
+print(bool(0))
+print(bool(""))
+print(bool(" "))
+print(bool("0"))
+
+print('Witam w moim swiecie')
