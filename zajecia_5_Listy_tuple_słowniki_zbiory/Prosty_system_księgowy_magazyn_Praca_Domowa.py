0512a916559e61e8f2c3bd82663a76a74f5bb638
witczgrzeg <grzegorz.witczak.gw@gmail.com>
2025-05-18 21:40:33 +0200

    skończona praca domowa zajęcia 5

--- zajecia_5_Listy_tuple_słowniki_zbiory/Prosty_system_księgowy_magazyn_Praca_Domowa.py
+++ zajecia_5_Listy_tuple_słowniki_zbiory/Prosty_system_księgowy_magazyn_Praca_Domowa.py
@@ -84,13 +84,15 @@ produkty_startowe = [
     {"nazwa": "plecak", "cena": 120, "ilosc": 4},
     {"nazwa": "rower", "cena": 1000, "ilosc": 2}
 ]
+historia= ["Spredaż: rower, 1000, 1 sztuk" , "Zakup: plecak, 120, 2 sztuk" ]
+
 
 saldo = 10000
 
 print("Witaj w systemie księgowo-magazynowym!")
 
 while True:
-    print("Wybierz jedną z poniższych opjci:")
+    print("Wybierz jedną z poniższych opjci podając numer 1-8:")
     komenda = input("""
     1. Salod
     2. Sprzedaż
@@ -110,22 +112,83 @@ while True:
                 print ("Saldo nie może być ujemne!")
             else:
                 saldo +=kwota
-                print(saldo)
 
         case "2":
-            print("pisać dakej tutaj z filmiku 1:01:07") # tutaj skonczylem ogladac filmik
+            nazwa = input(f"Podaj produkt który chcesz zamówić: \n")
+            produkt_znaleziony = False
+            for produkt in produkty_startowe:
+                if produkt.get("nazwa") == nazwa:
+                    produkt["ilosc"] -= 1
+                    cena = produkt["cena"]
+                    saldo += cena
+                    historia.append(f"Zakup: {produkt['nazwa']}, {produkt['cena']}, 1 sztuka")
+                produkt_znaleziony = True
+                break
+            if not produkt_znaleziony:
+                print("Tego produktu nie ma w asortymencie.")
+
+        case "3":
+            nazwa = input("Podaj nazwę produktu: \n")
+            cena = float(input("Podaj cenę produktu: \n"))
+            ilosc = int(input("Podaj ilość: \n"))
+            koszt = cena * ilosc
+
+            if saldo - koszt < 0:
+                print("Nie możesz sprzedać produktu ponieważ saldo będzie ujemne.")
+                historia.append(f"Próba zakupu: {nazwa}, {cena}, {ilosc} sztuk - nieudana.")
+                continue
+            else:
+                saldo -= koszt
+
+            produkt_znaleziony = False
+            for produkt in produkty_startowe:
+                if produkt["nazwa"] == nazwa:
+                    produkt["ilosc"] += ilosc
+                    produkt_znaleziony = True
+                    break
+
+            if not produkt_znaleziony:
+                produkty_startowe.append({
+                    "nazwa": nazwa,
+                    "cena": cena,
+                    "ilosc": ilosc
+                })
+
+            historia.append(f"Zakup: {nazwa}, {cena}, {ilosc} sztuk.")
+            print(f"Zakupiono {ilosc} szt. produktu {nazwa} za {koszt} zł.")
+
+        case "4":
+            print(f"Bierzące środki na koncie: {saldo}")
+
+        case "5":
+            print(f"Całkowity stan magazynu: {produkty_startowe}")
+
+        case "6":
+            nazwa = input("Podaj nazwę produktu, aby sprawdzić jego stan w magazynie: \n")
+            znaleziony = False
+
+            for produkt in produkty_startowe:
+                if produkt["nazwa"] == nazwa:
+                    print(f"Produkt: {produkt['nazwa']}, Ilość: {produkt['ilosc']}, Cena: {produkt['cena']} zł")
+                    znaleziony = True
+                    break
+
+            if not znaleziony:
+                print(f"Produkt o nazwie '{nazwa}' nie znajduje się w magazynie.")
+
+        case "7":
+            od = input("Podaj waartość od (numer transakcji): ")
+            do = input("Podaj wartość do (numer transakcji): ")
+            if od:
+                od=int(od)
+            else:
+                od = 0
+            if do:
+                do=int(do)
+            else:
+                do = len(historia)
+            print(historia[od:do])
+
         case "8":
             print("Zakończono działanie programu.")
             break
-        #lub
-        # if komenda in ("8", "koniec"):
-        #     print("Zakończono działanie programu.")
-        #     break
-
-# for nazwa in produkty_startowe:
-#     magazyn= print(nazwa)
-#
-# produkty_startowe.append({"nazwa": "woda", "cena": 3, "ilosc": 15})# tak mogę dodać towar do listy!
-#
-# for nazwa in produkty_startowe:
-#     magazyn= print(f"dodanie wody: {nazwa}")
