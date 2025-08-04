c10560cee2aea2a317efd87bcab76c8d80731f5b
witczgrzeg <grzegorz.witczak.gw@gmail.com>
2025-08-04 16:50:03 +0200

    koniec zajeć flask cz1

--- Aplikacja_Webowa_FLASK/zajecia_3_FLASK_wprowadzenie/app.py
+++ Aplikacja_Webowa_FLASK/zajecia_3_FLASK_wprowadzenie/app.py
@@ -1,37 +1,39 @@
 from flask import Flask, render_template, request, redirect
 
+
 users = [
     {
-        "name": "Anna Kowalska",
+        "name": "Anna",
         "age": 28,
         "email": "anna.kowalska@example.com",
         "city": "Warszawa"
     },
     {
-        "name": "Marek Nowak",
+        "name": "Marek",
         "age": 35,
         "email": "marek.nowak@example.com",
         "city": "Kraków"
     },
     {
-        "name": "Julia Wiśniewska",
+        "name": "Kasia",
         "age": 22,
-        "email": "julia.wisniewska@example.com",
+        "email": "kasia.zielinska@example.com",
         "city": "Gdańsk"
     },
     {
-        "name": "Tomasz Wójcik",
-        "age": 41,
-        "email": "tomasz.wojcik@example.com",
+        "name": "Tomasz",
+        "age": 40,
+        "email": "tomasz.lewandowski@example.com",
         "city": "Wrocław"
     },
     {
-        "name": "Magdalena Zielińska",
-        "age": 30,
-        "email": "magdalena.zielinska@example.com",
+        "name": "Alicja",
+        "age": 31,
+        "email": "alicja.kwiatkowska@example.com",
         "city": "Poznań"
     }
 ]
+
 
 
 app= Flask(__name__)
@@ -50,16 +52,27 @@ def hello_user(name=None):
 
 @app.route("/users", methods=["GET","POST"])
 def show_users():
+    saldo = 10
     if request.method == "POST":
-        new_user = {
-        "name": request.form.get("name"),
-        "age": request.form.get("age"),
-        "email": request.form.get("email"),
-        "city": request.form.get("city_name")
-        }
-        users.append(new_user)
-        return redirect("/")
-    return render_template("users.html", users=users)
+        form_type= request.form.get("form_type")
+        match form_type:
+            case "change_price":
+                saldo = request.form.get("new_saldo")
+            case "add_user":
+                new_user = {
+                        "name": request.form.get("name"),
+                        "age": request.form.get("age"),
+                        "email": request.form.get("email"),
+                        "city": request.form.get("city")
+                        }
+                users.append(new_user)
+            case "delete_user":
+                user_name = request.form.get("delete_name")
+                for user in users:
+                    if user["name"] == user_name:
+                        users.remove(user)
+                        break
+    return render_template("users.html", users=users, saldo=saldo)
 
 if __name__ == "__main__":
     app.run(debug=True)
