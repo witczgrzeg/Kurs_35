ad5630d2c06f87b53390f4803438a90080069df0
witczgrzeg <grzegorz.witczak.gw@gmail.com>
2025-08-08 01:42:13 +0200

    praca domowa SQL

--- Aplikacja_Webowa_FLASK/zajecia_5_SQLAlchemy_&_obsługa_bazy_danych/app.py
+++ Aplikacja_Webowa_FLASK/zajecia_5_SQLAlchemy_&_obsługa_bazy_danych/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///zajecia_25_baza.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,8 +16,27 @@ class User(db.Model):
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 with app.app_context():
     db.create_all()
+
+    #OPERACJA TWORZENIA NOWEGO REKORDU W BAZIE DANYCH
+    # user=User(
+    #     username='admin',
+    #     password='admin123',
+    #     first_name='Admin',
+    #     last_name='User',
+    # )
+    # db.session.add(user)
+    # db.session.commit()
+
+    users =User.query.all()
+    print(users)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- praca_zaliczeniowa/air_quality_app/app.py
+++ praca_zaliczeniowa/air_quality_app/app.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from routes import main_page
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.register_blueprint(main_page)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///AirQ.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-class User(db.Model):
+class LocationCity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(80), nullable=False)
-    location_name = db.Column(db.String(80), nullable=False)
+    location_name = db.Column(db.String(80), unique=True, nullable=False)
     sensor_id = db.Column(db.String(80), nullable=False)
     parameter= db.Column(db.String(80), nullable=False)
     value= db.Column(db.String(80), nullable=False)
@@ -18,6 +20,10 @@ class User(db.Model):
     date = db.Column(db.String(80), nullable=False)
     rating = db.Column(db.String(80), nullable=False)
 
+
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
