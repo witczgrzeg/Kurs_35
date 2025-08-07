6bf0522077389300b4eaf6e22795011f1f235fc3
witczgrzeg <grzegorz.witczak.gw@gmail.com>
2025-08-07 16:40:29 +0200

    zajecia z SQL: update projektu o baze danych

--- Aplikacja_Webowa_FLASK/zajecia_5_SQLAlchemy_&_obsługa_bazy_danych/app.py
+++ Aplikacja_Webowa_FLASK/zajecia_5_SQLAlchemy_&_obsługa_bazy_danych/app.py
@@ -1,9 +1,21 @@
-owoce = ['jabłko', 'banan', 'gruszka']
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-for owoc in owoce:
-    print(owoc)
 
-tekst = "Python"
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///zajecia_25_baza.db'
+db = SQLAlchemy(app)
 
-for litera in tekst:
-    print(litera)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
--- praca_zaliczeniowa/air_quality_app/app.py
+++ praca_zaliczeniowa/air_quality_app/app.py
@@ -1,8 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from routes import main_page
 
 app = Flask(__name__)
 app.register_blueprint(main_page)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///AirQ.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(80), nullable=False)
+    location_name = db.Column(db.String(80), nullable=False)
+    sensor_id = db.Column(db.String(80), nullable=False)
+    parameter= db.Column(db.String(80), nullable=False)
+    value= db.Column(db.String(80), nullable=False)
+    units = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.String(80), nullable=False)
+    rating = db.Column(db.String(80), nullable=False)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
